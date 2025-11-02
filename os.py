b57eb610c129278b9e3cea164e194c371cbe6596
hajar-al-attas <hajaralhashmia@gmail.com>
2025-11-02 19:30:39 +0300

    system information about OS

--- os.py
+++ os.py
@@ -2,20 +2,34 @@ import os
 import platform
 import string
 
-def system_info():
-    if os.name == "nt":
-        available_drives = [f"{d}:" for d in string.ascii_uppercase if os.path.exists(f"{d}:")]
+nameOS = platform.system()
+
+def disks():
+    if nameOS == "Windows":
+        return [f"{d}:" for d in string.ascii_uppercase if os.path.exists(f"{d}:")]
+    elif nameOS == "Linux" :
+        return os.popen("lsblk -d -n -o NAME").read().splitlines()
+    elif nameOS == "Darwin":
+        return os.popen("diskutil list").read().splitlines()
     else:
-        available_drives = ["Not supported on this OS"]
+        return "UnKnowin OS"
+    
+
+def root_path():
+    return os.environ.get('SYSTEMROOT',os.path.abspath(os.sep))
+
+            
+def system_info():
+
+    system_directory = root_path()
+    available_drives = disks()
 
     print("Operating System Information:")
-    print(f"OS Name: {platform.system()}")
+    print(f"OS Name: {nameOS}")
     print(f"Username: {os.getlogin()}")
-    print(f"System Directory: {os.path.abspath(os.sep)}")
-    print(f"System Directory: {os.environ.get('SYSTEMROOT', 'N/A')}")
-    print(f"CPU Count: {os.environ.get("NUMBER_OF_PROCESSORS", "Unknown")}")
+    print(f"System Directory: {system_directory}")
     print(f"CPU Count: {os.cpu_count()}")
-    print(f"Available Drives: {available_drives}")
+    print(f"Available Drives : {available_drives}")
 
 
 system_info()
